26a1444644c4bed63e107ea8cd17bca26c7d7345
statizarm <statiz.arm@gmail.com>
2020-11-08 16:24:36 +0300

    feat (http): jsonHandler function implementation finished

--- src/main.go
+++ src/main.go
@@ -5,12 +5,44 @@ import (
 	"net/http"
 	"log"
 	"io"
+	"golang.org/x/text/encoding/charmap"
 )
 
+func jsonMaker (runeCh <-chan rune, errorCount *uint32, doneCh chan<- bool) {
+	<- runeCh
+	*errorCount += 1
+	doneCh <- true
+}
+
 func jsonHandler (w http.ResponseWriter, r *http.Request) {
-	if _, err := io.Copy (w, r.Body); err != nil {
-		fmt.Printf ("%s\n", err);
+	var runeCh = make (chan rune, 20)
+	var doneCh = make (chan bool)
+
+	var errorCount uint32 = 0
+
+	go jsonMaker (runeCh, &errorCount, doneCh)
+
+	for {
+		var bytes = make ([]byte, 16)
+		n, err := r.Body.Read (bytes);
+
+		for i := 0; i < n; i += 1 {
+			runeCh <- charmap.Windows1251.DecodeByte (bytes[i])
+		}
+
+		if err != nil {
+
+			if err != io.EOF {
+				fmt.Printf ("%s\n", err.Error ())
+			}
+
+			break
+		}
 	}
+
+	<- doneCh
+
+	fmt.Fprintf (w, "%d\n", errorCount)
 }
 
 func main () {
